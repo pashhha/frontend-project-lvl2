bd0b5922dd6a2bcc9a3ecf8b9e649e46093f53e9
pavilskuzmins <pashhha@users-MacBook-Air.local>
2021-05-05 01:19:57 +0300

    gendiff updates

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,9 @@ const programm = require('commander');
 programm.version('1.0.0')
   .description('Compares two configuration files and shows a difference.');
 
+programm.usage('[options] <filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
+
 programm.parse(process.argv);
